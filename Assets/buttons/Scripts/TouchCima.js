43f9ba56e505d48f19d6a6c3857be76024a75e53
Wellington A. A. Silverio <wellington.silverio@etec.sp.gov.br>
2014-09-10 18:07:54 -0300

    Botoes

---
+++ Assets/buttons/Scripts/TouchCima.js
@@ -0,0 +1,35 @@
+﻿#pragma strict
+
+static var click = false;
+
+function Start () {
+
+}
+
+function Update () {
+	for(var i:int =0; i < Input.touches.Length; i++){ // quantos toques voce tem ??
+
+	var touch:Touch=Input.touches[i]; //o toque
+	var ray = Camera.main.ScreenPointToRay (Input.GetTouch(i).position);
+	var hit:RaycastHit =new RaycastHit ();
+
+	if(Physics.Raycast(ray,hit,1000)){
+		if(hit.collider.gameObject ==this.gameObject){
+		
+		switch (touch.phase) {
+			
+			case TouchPhase.Began: // se o toque começar
+				click = true; 
+				break;
+			
+			case TouchPhase.Ended: // se o toque começar
+				click = false;  
+				break;
+				
+	   }
+	  
+	  }
+
+	 }
+	 }
+}
