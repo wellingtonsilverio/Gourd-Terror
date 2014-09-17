4ddc43975005868e3a886ebfbfa30307921403ab
Wellington A. A. Silverio <wellington.silverio@etec.sp.gov.br>
2014-09-17 18:57:27 -0300

    Fogueira

---
+++ Assets/Controller.js
@@ -0,0 +1,23 @@
+﻿#pragma strict
+
+var velAndar = 10;
+
+function Start () {
+
+}
+
+function Update () {
+	if(TouchEsquerda.click == true){
+		transform.Translate(velAndar,0,0);
+	}
+	if(TouchDireita.click == true){
+		transform.Translate(velAndar*-1,0,0);
+	}
+	if(TouchCima.click == true){
+		transform.Translate(0,velAndar,0);
+	}
+	if(TouchBaixo.click == true){
+		transform.Translate(0,velAndar*-1,0);
+	}
+
+}
